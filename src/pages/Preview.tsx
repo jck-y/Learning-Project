e4ef26df63867a4eefb7961cd6f26cf226290e1e
jck-y <j13karonngkong@gmail.com>
2026-05-28 14:17:13 +0800

    16

--- src/pages/Preview.tsx
+++ src/pages/Preview.tsx
@@ -1,31 +1,30 @@
-
 import { useEffect, useState } from "react";
 
-interface ImageResult {
+interface AppResult {
   title: string;
-  original: string;
-  source: string;
+  thumbnail: string;
+  rating: number;
+  author: string;
+  category: string;
+  downloads: string;
+  link: string;
 }
 interface SerpApiResponse {
-  images_results: ImageResult[];
+  organic_results: {
+    items: AppResult[];
+  }[];
 }
 
-
 function Preview() {
-    // const [data, setData] = useState(null)
     const [data, setData] = useState<SerpApiResponse | null>(null);
-    // const API_KEY = "7667d67efe0280c481ede11ce66d6fcb3e87f3081e6bd79166be908245621066"
-    // const ENDPOINT = "https://serpapi.com/search"
 
     const params = {
-        engine: "google_images",
-        q: "dog",
+        engine: "google_play",
+        q: "face",
         hl: "id",
     }
+
     useEffect(() => {
-        // const queryString = new URLSearchParams({ ...params, api_key: API_KEY }).toString()
-        // const serpUrl = `${ENDPOINT}?${queryString}`
-        // fetch("https://corsproxy.io/?" + encodeURIComponent(serpUrl))
         const queryString = new URLSearchParams(params).toString()
         fetch(`/api/search?${queryString}`)
             .then((res) => res.json())
@@ -36,15 +35,15 @@ function Preview() {
             .catch((err) => console.error("Error:", err))
     }, [])
 
-    if (!data) {
-        return <h1>Loading...</h1>
-    }
+    if (!data) return <h1>Loading...</h1>
 
     return (
         <div>
-            <h2>{data.images_results[1].title}</h2>
-            <img src={data.images_results[1].original} width="400" />
-            <p>Sumber: {data.images_results[1].source}</p>
+            <h2>{data.organic_results[0].items[0].title}</h2>
+            <img src={data.organic_results[0].items[0].thumbnail} width="200" />
+            <p>Developer: {data.organic_results[0].items[0].author}</p>
+            <p>Rating: {data.organic_results[0].items[0].rating}</p>
+            <p>Download: {data.organic_results[0].items[0].downloads}</p>
         </div>
     )
 }
